b0c6395078a71a9c6c40c56d0fc9b242cb4bec96
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-09 14:47:45 +0530

    Update imgtovid.py

--- plugins/imgtovid.py
+++ plugins/imgtovid.py
@@ -1,1 +1,10 @@
+from moviepy.editor import AudioFileClip, ImageClip
+import imageio
+
+def merge_image_and_audio(image_path, audio_path, output_path, fps=24):
+    audio = AudioFileClip(audio_path)
+    image = imageio.imread(image_path)
+    image_clip = ImageClip(image).set_duration(audio.duration).set_fps(fps)
+    final_clip = image_clip.set_audio(audio)
+    final_clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
 
