43aca2801466b1e88f128eb129d896331ee48ce0
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-09 14:53:21 +0530

    Update merge_vid.py

--- plugins/merge_vid.py
+++ plugins/merge_vid.py
@@ -1,1 +1,7 @@
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+def join_videos(video_paths, output_path):
+    clips = [VideoFileClip(path) for path in video_paths]
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
 
