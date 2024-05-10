deaad2572ffe57c2c10e63c8b7fb94f323e5fcd7
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-10 20:47:32 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -2,32 +2,19 @@ import getpass
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
+from PIL import Image 
 
-
-def get_gemini_response(SystemMessage, HumanMessage, image=None):
+def get_gemini_response(prompt, image=None):
     if not image:
         if "GOOGLE_API_KEY" not in os.environ:
             os.environ["GOOGLE_API_KEY"] = getpass.getpass("Gemini API KEY is missing in environment.")
-      
-        llm = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
-        result = llm.invoke(
-                     [
-                         SystemMessage(content=SystemMessage),
-                         HumanMessage(content=HumanMessage),
-                     ]
-                 )
+        llm = ChatGoogleGenerativeAI(model="gemini-pro")
+        result = llm.invoke(prompt)
         return result.content
-    img = None
     if image:
         if "GOOGLE_API_KEY" not in os.environ:
             os.environ["GOOGLE_API_KEY"] = getpass.getpass("Gemini API KEY is missing in environment.")
-        with Image.open(image) as im:
-            img = im
-        llm = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
-        result = llm.invoke(
-                     [
-                         SystemMessage(content=SystemMessage),
-                         HumanMessage(content=[{"type": "text", "text": "What's in this image?"}, {"type": "image", "image": img}]),
-                     ]
-                 )
-        return result.content
+        with Image.open(image) as img:
+            llm = ChatGoogleGenerativeAI(model="gemini-pro")
+            result = llm.invoke(prompt, img)
+            return result.content
