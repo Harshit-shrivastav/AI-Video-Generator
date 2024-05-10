4c8ca56e6db4965dbe86ceb89edc4cbcb088b3cc
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-10 18:52:41 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -10,25 +10,24 @@ def get_gemini_response(SystemMessage, HumanMessage, image=None):
             os.environ["GOOGLE_API_KEY"] = getpass.getpass("Gemini API KEY is missing in environment.")
       
         llm = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
-        result = model.invoke(
+        result = llm.invoke(
                      [
                          SystemMessage(content=SystemMessage),
                          HumanMessage(content=HumanMessage),
                      ]
                  )
         return result.content
+    img = None
     if image:
         if "GOOGLE_API_KEY" not in os.environ:
             os.environ["GOOGLE_API_KEY"] = getpass.getpass("Gemini API KEY is missing in environment.")
-      
+        with Image.open(image) as im:
+            img = im
         llm = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
-        result = model.invoke(
+        result = llm.invoke(
                      [
                          SystemMessage(content=SystemMessage),
-                         HumanMessage(content=[ {"type": "text", "text": "What's in this image?",}, {"type": "image", "image": image}]),
+                         HumanMessage(content=[{"type": "text", "text": "What's in this image?"}, {"type": "image", "image": img}]),
                      ]
                  )
         return result.content
-
-
-
