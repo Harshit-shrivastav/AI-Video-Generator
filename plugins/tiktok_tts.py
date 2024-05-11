38b089778b80ac737c4fd6b4c7b3f7e98c7ef03b
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-11 14:13:14 +0530

    Update tiktok_tts.py

--- plugins/tiktok_tts.py
+++ plugins/tiktok_tts.py
@@ -4,13 +4,15 @@ import base64
 ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev'
 TEXT_BYTE_LIMIT = 300
 
-def get_tt_tts(text, speaker="en_us_001"):
+def get_tt_tts(text, output_file_path, speaker="en_us_001"):
     try:
         response = requests.post(f"{ENDPOINT}/api/generation", json={"text": text, "voice": speaker})
         if response.status_code == 200:
             data = response.json()
             if data.get('data'):
+                # Decode base64-encoded audio data
                 audio_data = base64.b64decode(data['data'])
+                save_audio(audio_data, output_file_path)
                 return audio_data, speaker
             else:
                 print(f"Generation failed: {data.get('error', 'Unknown error')}")
@@ -22,10 +24,10 @@ def get_tt_tts(text, speaker="en_us_001"):
         print(f"Error generating audio: {e}")
         return None, None
 
-def save_audio(audio_data):
+def save_audio(audio_data, output_file_path):
     try:
-        with open("audio.mp3", "wb") as f:
+        with open(output_file_path, "wb") as f:
             f.write(audio_data)
-        print("Audio saved successfully as 'audio.mp3'")
+        print(f"Audio saved successfully as '{output_file_path}'")
     except Exception as e:
         print(f"Error saving audio: {e}")
