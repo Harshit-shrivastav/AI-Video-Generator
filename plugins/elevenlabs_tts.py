1ae5d1b07d573e8014b0d21e9a3fcae61c69e022
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-11 14:06:31 +0530

    Create elevenlabs_tts.py

---
+++ plugins/elevenlabs_tts.py
@@ -0,0 +1,15 @@
+from elevenlabs import play
+from elevenlabs.client import ElevenLabs
+
+client = ElevenLabs(
+  api_key="YOUR_API_KEY",
+)
+
+audio = client.generate(
+  text="Hello! 你好! Hola! नमस्ते! Bonjour! こんにちは! مرحبا! 안녕하세요! Ciao! Cześć! Привіт! வணக்கம்!",
+  voice="Rachel",
+  model="eleven_multilingual_v2"
+)
+play(audio)
+
+def get_el
