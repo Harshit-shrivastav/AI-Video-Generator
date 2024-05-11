670bbb19c181c41042f545889f1bd49aee417879
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-11 19:58:31 +0530

    Update elevenlabs_tts.py

--- plugins/elevenlabs_tts.py
+++ plugins/elevenlabs_tts.py
@@ -4,9 +4,8 @@ import os
 
 API_KEY = os.environ.get("ELEVENLABS_API_KEY")
 
-
 def get_elevenlabs_tts(Text , Voice):
-    if "GOOGLE_API_KEY" in os.environ:
+    if "ELEVENLABS_API_KEY" in os.environ:
         client = ElevenLabs(api_key=API_KEY)
         audio = client.generate(text=Text, voice = Voice, model="eleven_multilingual_v2")
         return audio 
