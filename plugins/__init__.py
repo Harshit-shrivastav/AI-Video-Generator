424d8fd3efccc8183befe89499dec326105b7a36
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-11 20:36:50 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -11,7 +11,7 @@ title = str(input("Please enter a title to get started")
 llm_response = get_llm_response(title)
 
 background_image = generate_background_image(1600, 900, (255, 255, 255), 10, (0, 0, 0))
-written_text, extra_text = write_text_on_image(background_image, '''this is another sentence''', "temps/slides/slide.png")
+written_text, extra_text = write_text_on_image(background_image, llm_response, "temps/slides/slide.png")
 
 ask_tts = int(input("which tts service do you want to use\n1. ElevenLabs \n2. Edge\n3. TikTok")
               
@@ -19,7 +19,7 @@ if ask_tts == 1:
     pass
 elif ask_tts == 2:
     get_edge_tts(written_text, "temps/voices/voice.mp3"):
-else:
+elif ask_tts == 3 :
     get_tt_tts(written_text, "temps/voices/voice.mp3")
 
 merge_image_and_audio(image_path, audio_path, output_path)
