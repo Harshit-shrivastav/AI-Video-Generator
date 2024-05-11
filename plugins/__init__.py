62a5275badfc46a7af4bb1738fd98d10b277aae8
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-11 20:05:00 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,6 @@
-
+from .slide import generate_background_image, write_text_on_image
+from .tiktok_tts import get_tt_tts
+from .merge_vid import join_videos
+from .imgtovid import merge_image_and_audio
+from .elevenlabs_tts import get_elevenlabs_tts
+from .edge_tts import get_edge_tts
