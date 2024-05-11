1d8242891126306ae985e1ce8d8491709c2db430
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-11 14:11:19 +0530

    Update tiktok_tts.py

--- plugins/tiktok_tts.py
+++ plugins/tiktok_tts.py
@@ -1,17 +1,15 @@
 import requests
-import json
 import base64
 
 ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev'
 TEXT_BYTE_LIMIT = 300
 
-def generate_tts(text, speaker="en_us_001"):
+def get_tt_tts(text, speaker="en_us_001"):
     try:
         response = requests.post(f"{ENDPOINT}/api/generation", json={"text": text, "voice": speaker})
         if response.status_code == 200:
             data = response.json()
             if data.get('data'):
-                # Decode base64-encoded audio data
                 audio_data = base64.b64decode(data['data'])
                 return audio_data, speaker
             else:
@@ -31,5 +29,3 @@ def save_audio(audio_data):
         print("Audio saved successfully as 'audio.mp3'")
     except Exception as e:
         print(f"Error saving audio: {e}")
-
-
