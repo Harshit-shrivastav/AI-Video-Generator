2e90106e9bb8b001d062a246cd3202e72e74e6fc
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-12 16:18:00 +0530

    Create app.py

---
+++ plugins/app.py
@@ -0,0 +1,35 @@
+from slide import generate_background_image, write_text_on_image
+from tiktok_tts import get_tt_tts
+from merge_vid import join_videos
+from imgtovid import merge_image_and_audio
+from elevenlabs_tts import get_elevenlabs_tts
+from edge_tts import get_edge_tts
+from model import get_llm_response
+
+title = str(input("Please enter a title to get started: "))
+ask_tts = int(input("Which TTS service do you want to use?\n1. ElevenLabs \n2. Edge\n3. TikTok: "))
+speaker = str(input("Enter a speaker name: "))
+
+llm_response = get_llm_response(title)
+background_image = generate_background_image(1600, 900, (255, 255, 255), 10, (0, 0, 0))
+slide, written_text, extra_text = write_text_on_image(background_image, llm_response)
+
+videos = []
+
+while extra_text:
+    voice = ''
+    if ask_tts == 1:
+        voice = get_elevenlabs_tts(written_text, speaker)
+    elif ask_tts == 2:
+        voice = get_edge_tts(written_text)
+    elif ask_tts == 3:
+        voice = get_tt_tts(written_text)
+
+    vid = merge_image_and_audio(slide, voice)
+    videos.append(vid)
+    slide, written_text, extra_text = write_text_on_image(background_image, llm_response)
+else:
+    vid = merge_image_and_audio(slide, voice)
+    videos.append(vid)
+
+join_videos(videos, "results/finalvideo.mp4")
