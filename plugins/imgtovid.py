9975d1ccded350a2cf429c0ce456b49354b68a03
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-18 00:30:43 +0530

    Update imgtovid.py

--- plugins/imgtovid.py
+++ plugins/imgtovid.py
@@ -2,12 +2,14 @@ from moviepy.editor import AudioFileClip, ImageClip
 import imageio
 
 def merge_image_and_audio(image_path, audio_path, fps=24):
-    audio = AudioFileClip(audio_path)
-    image = imageio.imread(image_path)
-    image_clip = ImageClip(image).set_duration(audio.duration).set_fps(fps)
-    final_clip = image_clip.set_audio(audio)
-    return final_clip
-
+    if image_path and audio_path:
+        audio = AudioFileClip(audio_path)
+        image = imageio.imread(image_path)
+        image_clip = ImageClip(image).set_duration(audio.duration).set_fps(fps)
+        final_clip = image_clip.set_audio(audio)
+        return final_clip
+    else:
+        return False 
 # Example usage:
 #final_video_clip = merge_image_and_audio("image.jpg", "audio.mp3")
 #final_video_clip.preview()
