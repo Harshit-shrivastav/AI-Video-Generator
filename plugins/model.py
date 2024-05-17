a5917b03ca2f74dfbca49fd633555e8069517f7f
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-17 19:32:18 +0530

    Update model.py

--- plugins/model.py
+++ plugins/model.py
@@ -2,32 +2,30 @@ import requests
 import os
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAGe0LzEjcyRNZCtTMALDlsSRKsHLf_e84")
+GOOGLE_API_KEY = "AIzaSyAGe0LzEjcyRNZCtTMALDlsSRKsHLf_e84"
+
+genai.configure(api_key=GOOGLE_API_KEY)
 
 
 def get_llm_response(Prompt, image=None):
     prompt = f'''{Prompt}'''
-    if image and "GOOGLE_API_KEY" in os.environ:
+    if image and GOOGLE_API_KEY:
         if prompt:
             img = Image.open(image)
             llm = genai.GenerativeModel('gemini-pro-vision')
             if img:
                 response = llm.generate_content([prompt, img])
-                print(response.text)
                 return response.text
             else:
-                return False
-    elif (image and "GOOGLE_API_KEY" not in os.environ):
-        print("Please set GOOGLE_API_KEY as environment variable, to use image.")
-        return False
-    elif (not image and "GOOGLE_API_KEY" not in os.environ):
-        print("GOOGLE_API_KEY is missing")
-        return False 
-    elif (not image and "GOOGLE_API_KEY" in os.environ):
+                return False 
+    elif not image and GOOGLE_API_KEY:
         if prompt:
             llm = genai.GenerativeModel('gemini-pro')
             response = llm.generate_content(prompt)
-            print(response.text)
             return response.text
+    elif not GOOGLE_API_KEY:
+        print("GOOGLE_API_KEY is missing")
+        return False
     else:
-        return False 
+        print("Unconditional Exception")
+        return False
