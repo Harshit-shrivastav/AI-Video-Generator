9d2ddf5e2a524d5e747ab6ad94c1609aede3e6b6
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-17 19:23:19 +0530

    Update model.py

--- plugins/model.py
+++ plugins/model.py
@@ -1,27 +1,8 @@
 import requests
 import os
 import google.generativeai as genai
-from PIL import Image 
-
 
 genai.configure(api_key="AIzaSyAGe0LzEjcyRNZCtTMALDlsSRKsHLf_e84")
-
-
-def chatgpt(msg):
-    url = f'https://api.voidevs.com/gpt/free?msg={msg}'
-    try:
-        respons = requests.get(url)
-        if respons.status_code == 200:
-            response_text = respons.text
-            response_dict = json.loads(response_text)
-            if response_dict['result']:
-                return response_dict['results']['answer']
-            else:
-                return False
-        else:
-            return False
-    except:
-        return False
 
 
 def get_llm_response(Prompt, image=None):
@@ -40,12 +21,8 @@ def get_llm_response(Prompt, image=None):
         print("Please set GOOGLE_API_KEY as environment variable, to use image.")
         return False
     elif (not image and "GOOGLE_API_KEY" not in os.environ):
-        if prompt:
-            result = chatgpt(prompt)
-            if result:
-                return result
-            else:
-                return False 
+        print("GOOGLE_API_KEY is missing")
+        return False 
     elif (not image and "GOOGLE_API_KEY" in os.environ):
         if prompt:
             llm = genai.GenerativeModel('gemini-pro')
