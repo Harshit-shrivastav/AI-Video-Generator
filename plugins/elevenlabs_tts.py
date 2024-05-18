b1fa6cb5ccc500994494c9fbbe69a642e7fedb01
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-18 17:17:58 +0530

    Update elevenlabs_tts.py

--- plugins/elevenlabs_tts.py
+++ plugins/elevenlabs_tts.py
@@ -4,11 +4,11 @@ import os
 
 API_KEY = os.environ.get("ELEVENLABS_API_KEY")
 
-def get_elevenlabs_tts(Text , Voice):
-    if "ELEVENLABS_API_KEY" in os.environ:
+def get_elevenlabs_tts(text, voice):
+    if API_KEY:
         client = ElevenLabs(api_key=API_KEY)
-        audio = client.generate(text=Text, voice = Voice, model="eleven_multilingual_v2")
-        return audio 
+        audio = client.generate(text=text, voice=voice, model="eleven_multilingual_v2")
+        return audio
     else:
         print("ElevenLabs API KEY is not available in environment")
-        return False 
+        return None
