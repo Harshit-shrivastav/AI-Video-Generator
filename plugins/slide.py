b345173e2874404b557cf7bab42cd39e2f07d296
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-18 12:15:48 +0530

    Update slide.py

--- plugins/slide.py
+++ plugins/slide.py
@@ -1,11 +1,9 @@
 import requests
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
+import numpy as np
 
 def generate_background_image(width, height, background_color, border_width, border_color):
-    """
-    Generates a background image with a specified width, height, background color, and border.
-    """
     image = Image.new('RGBA', (width, height), background_color + (255,))
     border_top_bottom = Image.new('RGBA', (width, border_width), border_color + (255,))
     border_left_right = Image.new('RGBA', (border_width, height), border_color + (255,))
@@ -18,9 +16,6 @@ def generate_background_image(width, height, background_color, border_width, bor
     return image
 
 def load_font(font_path, font_size):
-    """
-    Loads a font from a given path. If the path is a URL, it downloads the font first.
-    """
     if font_path.startswith("http://") or font_path.startswith("https://"):
         response = requests.get(font_path)
         font = ImageFont.truetype(BytesIO(response.content), font_size)
@@ -29,18 +24,13 @@ def load_font(font_path, font_size):
     return font
 
 def write_text_on_image(background_image, text, font_path="https://github.com/Harshit-shrivastav/ai-video-generator/raw/v2/assets/fonts/RobotoCondensed-Black.ttf", font_size=100, text_color=(0, 0, 0)):
-    """
-    Writes complete sentences on the background image starting from the upper-left corner with a margin of 100 pixels from the edges.
-    The text after the last full stop is not written on the image.
-    Returns the image, the extra text that couldn't be written (or None if there's no extra text), and the text that was written on the image.
-    """
     width, height = background_image.size
     draw = ImageDraw.Draw(background_image)
     font = load_font(font_path, font_size)
     text_x = 100
     text_y = 100
     line_width = 0
-    max_height = height - 100 - font_size  # Maximum height for text with a 100-pixel margin at the bottom and considering font height
+    max_height = height - 100 - font_size
     words = text.split()
     remaining_text = []
     written_text = []
@@ -66,7 +56,7 @@ def write_text_on_image(background_image, text, font_path="https://github.com/Ha
             continue
 
         if word.endswith('.'):
-            last_period_index = i + 1  # Store the index of the word after the last period
+            last_period_index = i + 1
 
         if line_width + word_width >= width - 200:
             text_y += font_size
@@ -90,11 +80,3 @@ def write_text_on_image(background_image, text, font_path="https://github.com/Ha
         extra_text = None
 
     return background_image, extra_text, written_text_str
-
-
-"""
-background_image = generate_background_image(1600, 900, (255, 255, 255), 10, (0, 0, 0))
-output_image, extra_text = write_text_on_image(background_image, '''this is another sentence''')
-output_image.show()  
-output_image.save("output.png")
-"""
