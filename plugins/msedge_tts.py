7ee18934a43e9179e0473bf5cf8e1a1f622d886d
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-18 14:28:00 +0530

    Update msedge_tts.py

--- plugins/msedge_tts.py
+++ plugins/msedge_tts.py
@@ -5,90 +5,11 @@ from concurrent.futures import ThreadPoolExecutor
 import edge_tts
 from edge_tts import VoicesManager
 
-def get_edge_tts(text, language='english'):
-    language_mapping = {
-        'english': 'en',
-        'persian': 'fa',
-        'vietnamese': 'vi',
-        'zulu': 'zu',
-        'afrikaans': 'af',
-        'amharic': 'am',
-        'arabic': 'ar',
-        'azerbaijani': 'az',
-        'bulgarian': 'bg',
-        'bengali': 'bn',
-        'bosnian': 'bs',
-        'catalan': 'ca',
-        'czech': 'cs',
-        'welsh': 'cy',
-        'danish': 'da',
-        'german': 'de',
-        'greek': 'el',
-        'spanish': 'es',
-        'estonian': 'et',
-        'filipino': 'fil',
-        'finnish': 'fi',
-        'french': 'fr',
-        'irish': 'ga',
-        'galician': 'gl',
-        'gujarati': 'gu',
-        'hebrew': 'he',
-        'hindi': 'hi',
-        'croatian': 'hr',
-        'hungarian': 'hu',
-        'indonesian': 'id',
-        'icelandic': 'is',
-        'italian': 'it',
-        'japanese': 'ja',
-        'javanese': 'jv',
-        'georgian': 'ka',
-        'kazakh': 'kk',
-        'khmer': 'km',
-        'kannada': 'kn',
-        'korean': 'ko',
-        'lao': 'lo',
-        'lithuanian': 'lt',
-        'latvian': 'lv',
-        'macedonian': 'mk',
-        'malayalam': 'ml',
-        'mongolian': 'mn',
-        'marathi': 'mr',
-        'malay': 'ms',
-        'maltese': 'mt',
-        'burmese': 'my',
-        'norwegian': 'nb',
-        'nepali': 'ne',
-        'dutch': 'nl',
-        'polish': 'pl',
-        'pashto': 'ps',
-        'portuguese': 'pt',
-        'romanian': 'ro',
-        'russian': 'ru',
-        'sinhala': 'si',
-        'slovak': 'sk',
-        'slovenian': 'sl',
-        'somali': 'so',
-        'albanian': 'sq',
-        'serbian': 'sr',
-        'sundanese': 'su',
-        'swedish': 'sv',
-        'swahili': 'sw',
-        'tamil': 'ta',
-        'telugu': 'te',
-        'thai': 'th',
-        'turkish': 'tr',
-        'ukrainian': 'uk',
-        'urdu': 'ur',
-        'uzbek': 'uz'
-    }
-
-    async def async_generate_audio(text, lang):
+def get_edge_tts(text):
+    async def async_generate_audio(text):
         try:
             voices = await VoicesManager.create()
-            if lang == "english":
-                voice = voices.find(Locale="en-US")
-            else:
-                voice = voices.find(Language=lang)
+            voice = voices.find(Locale="en-US")
             speaker = random.choice(voice)["Name"]
             communicate = edge_tts.Communicate(text, speaker)
             audio_data = b''
@@ -100,16 +21,15 @@ def get_edge_tts(text, language='english'):
             print("Error generating audio using edge_tts:", e)
             raise
 
-    async def run_async(text, lang):
+    async def run_async(text):
         try:
-            return await async_generate_audio(text, lang)
+            return await async_generate_audio(text)
         except Exception as e:
             print("An error occurred during audio generation:", e)
             raise
 
     try:
-        lang_code = language_mapping.get(language.lower(), 'en')
-        return asyncio.run(run_async(text, lang_code))
+        return asyncio.run(run_async(text))
     except Exception as e:
         print("Error generating text to speech:", e)
         return None
