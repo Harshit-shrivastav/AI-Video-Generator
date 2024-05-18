9d1d5e163bd41fefee141ddba4dc2a2df067a884
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-18 20:29:46 +0530

    Update elevenlabs_tts.py

--- plugins/elevenlabs_tts.py
+++ plugins/elevenlabs_tts.py
@@ -1,10 +1,11 @@
 from elevenlabs import play
 from elevenlabs.client import ElevenLabs
 import os
+import asyncio 
 
 API_KEY = os.environ.get("ELEVENLABS_API_KEY")
 
-def get_elevenlabs_tts(text, voice):
+async def get_elevenlabs_tts(text, voice):
     if API_KEY:
         client = ElevenLabs(api_key=API_KEY)
         audio = client.generate(text=text, voice=voice, model="eleven_multilingual_v2")
