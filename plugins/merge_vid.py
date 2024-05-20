4f0e3275cc2c509e2eb4c8c3e30b3ce1d0bd8f9e
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-20 22:00:39 +0530

    Update merge_vid.py

--- plugins/merge_vid.py
+++ plugins/merge_vid.py
@@ -4,18 +4,7 @@ from io import BytesIO
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
 def merge_videos(video_data_list, save_path):
-    """
-    Merges a list of video data into a single video.
-
-    Args:
-        video_data_list (list): List of video data in bytes.
-        save_path (str): Path to save the final video.
-
-    Returns:
-        None
-    """
     video_clips = []
-
     for video_data in video_data_list:
         temp_video_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
         temp_video_file.write(video_data)
