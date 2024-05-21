31fbc1c24b0a80c76947309f984384739705ad6e
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-21 08:52:05 +0530

    Update model.py

--- plugins/model.py
+++ plugins/model.py
@@ -1,25 +1,16 @@
-import requests
 import os
-from PIL import Image 
-import google.generativeai as genai
-from groq import Groq
 from llmware.prompts import Prompt
+from groq import Groq
 
 GROQ_API_KEY = os.environ.get('GROQ_API_KEY', 'Grig_api_key')
-GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', 'Grig_api_key')
-
-if GOOGLE_API_KEY:
-    genai.configure(api_key=GOOGLE_API_KEY)
-
 
 def get_response(user_prompt) -> str:
     prompter = Prompt().load_model("phi-3-gguf")
     output = prompter.prompt_main(
         user_prompt, prompt_name="default_with_context", temperature=0.30
     )
-    response = output["llm_response"].strip("\n").partition("<|end|>")[0]
+    response = output["llm_response"].strip("\n").partition("")[0]
     return response
-
 
 def get_groq_response(user_prompt, system_prompt):
     client = Groq(api_key=GROQ_API_KEY)
@@ -38,47 +29,20 @@ def get_groq_response(user_prompt, system_prompt):
     )
     return chat_completion.choices[0].message.content
 
-def get_llm_response(user_prompt, system_prompt, image=None):
+def get_llm_response(user_prompt, system_prompt):
     formatted_prompt = f"""
     {system_prompt}
     Topic: {user_prompt}
     Your Answer: 
     """
-
-    if image and GOOGLE_API_KEY:
-        try:
-            img = Image.open(image)
-            llm = genai.GenerativeModel('gemini-pro-vision')
-            response = llm.generate_content([formatted_prompt, img])
-            return response.text
-        except Exception as e:
-            print(f"Error generating response with image: {e}")
+    try:
+        response = get_response(formatted_prompt)
+        return response
+    except Exception as e:
+        print(f"Error generating response with LLMWare AI: {e}, trying with Groq if possible.")
+        if GROQ_API_KEY:
+            result = get_groq_response(user_prompt, system_prompt)
+            return result
+        else:
+            print("No Groq API key found.")
             return False
-    elif not image:
-        try:
-            response = get_response(formatted_prompt)
-            return response
-        except Exception as e:
-            print(f"Error generating response with LLMWare AI: {e}, trying with Groq if possible.")
-            if GROQ_API_KEY:
-                result = get_groq_response(user_prompt, system_prompt)
-                return result
-            else:
-                print("No Groq API key found.")
-                return False
-
-    elif not GOOGLE_API_KEY:
-        try:
-            response = get_response(formatted_prompt)
-            return response
-        except:
-            try:
-                result = get_groq_response(prompt)
-                return result
-            except Exception as e:
-                print(e)
-                return False
-            
-    else:
-        print("Unconditional Exception")
-        return False
