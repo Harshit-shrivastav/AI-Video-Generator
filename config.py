ebed1f555da40552174622189dfd0c0ccd525174
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-11-05 10:25:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,8 @@ import os
 from dotenv import load_dotenv 
 
 load_dotenv()
+
+DOMAIN = os.environ.get('DOMAIN', 'http://127.0.0.1:8080')
+PORT = os.environ.get('PORT', 8000)
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS', 'Email_here')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'Password')
